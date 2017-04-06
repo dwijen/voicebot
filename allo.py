fcb77945a03c1d333448f7a1cf9d19580033f06f
dwijen.b <dwijen.b@fireeye.com>
2017-04-06 22:11:22 +0530

    sencond commit

--- allo.py
+++ allo.py
@@ -15,7 +15,6 @@ if __name__ =='__main__':
     t2s = t2s.T2S()
 
     while True:
-        print("Tell Something")
         t2s.test2Speech("Tell Something")
         speechText = s2t.startListening()
         botReply = chatbot.ask(speechText)
